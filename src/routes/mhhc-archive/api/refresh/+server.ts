d0a8a67bdd1cd5c780bd7f5856ea2219758a7da6
legomolina <legomolina@gmail.com>
2024-03-11 19:17:31 +0100

    Created endpoint to manually refresh cards.

---
+++ src/routes/mhhc-archive/api/refresh/+server.ts
@@ -0,0 +1,34 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import type { CollectionModel, CollectionResponseModel } from '@domain/mhhc-archive/models/collection.model';
+import { GITHUB_WEBHOOK_SECRET } from '$env/static/private';
+import { PUBLIC_ARCHIVE_INFO_FILE } from '$env/static/public';
+import { error } from '@sveltejs/kit';
+import { buildCollection } from '@application/factories/mhhc-archive/collection.factory';
+import { fetchCard } from '@application/images/mhhc-archive/fetch-cards.service';
+
+export const GET: RequestHandler = async ({ url }) => {
+    const token = url.searchParams.get('t');
+    const collectionName = url.searchParams.get('c');
+
+    if (token !== GITHUB_WEBHOOK_SECRET || !collectionName) {
+        error(500);
+    }
+
+    const json: { cards: { collections: CollectionResponseModel[] } } = await fetch(PUBLIC_ARCHIVE_INFO_FILE).then(res => res.json());
+    const collection = json.cards.collections.map((collection) => buildCollection(collection)).find((collection) => collection.name === decodeURIComponent(collectionName));
+
+    const getCards = async (collection: CollectionModel) => {
+        for (const coverCard of collection.cards) {
+            try {
+                await fetchCard(coverCard, fetch, true);
+            } catch { /* empty */ }
+        }
+    }
+
+    if (collection) {
+        getCards(collection).then(() => console.log(`Refreshed cards from collection ${collection.name}`));
+        return new Response('Refreshing cards');
+    }
+
+    error(500, 'Collection not found');
+}
--- src/routes/mhhc-archive/api/webhook/+server.ts
+++ src/routes/mhhc-archive/api/webhook/+server.ts
@@ -1,11 +1,8 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import type { CollectionModel, CollectionResponseModel } from '@domain/mhhc-archive/models/collection.model';
 import { createHmac, timingSafeEqual } from 'crypto';
 import { GITHUB_WEBHOOK_SECRET, TEBI_MHHC_BUCKET_ID, TEBI_URL } from '$env/static/private';
 import { error } from '@sveltejs/kit';
 import { fetchCard } from '@application/images/mhhc-archive/fetch-cards.service';
-import { PUBLIC_ARCHIVE_INFO_FILE } from '$env/static/public';
-import { buildCollection } from '@application/factories/mhhc-archive/collection.factory';
 import type { CardModel } from '@domain/mhhc-archive/models/card.model';
 
 type Commit = {
@@ -14,71 +11,73 @@ type Commit = {
 };
 
 export const POST: RequestHandler = async ({ fetch, request }) => {
-    const checkValidity = (textBody: string) => {
-        if (!textBody) {
-            throw new Error('Unable validate signature');
-        }
-
-        const signatureHeaderName = 'X-Hub-Signature-256';
-        const signatureHashAlgorithm = 'sha256';
-        const signature = Buffer.from(request.headers.get(signatureHeaderName) ?? '', 'utf8');
-        const hmac = createHmac(signatureHashAlgorithm, GITHUB_WEBHOOK_SECRET);
-        const digest = Buffer.from(signatureHashAlgorithm + '=' + hmac.update(textBody).digest('hex'), 'utf8');
-
-        if (signature.length !== digest.length || !timingSafeEqual(digest, signature)) {
-            throw new Error('Unable validate signature');
-        }
-    }
-
-    const getFilesFromCommits = (commits: Commit[]): string[] => {
-        return commits.reduce((previousValue, currentValue) => {
-            return [
-                ...previousValue,
-                ...currentValue.modified,
-                ...currentValue.added,
-            ];
-        }, [] as string[]);
-    }
-
-    const buildCardFromFile = (file: string): CardModel => {
-        const splitted = file.split('/').filter(s => s !== '');
-        const cardName = splitted.at(-1)!;
-        const collectionName = splitted.at(-2)!;
-
-        const urlWithoutExtension = cardName.replace(/\.[^/.]+$/, "");
-        const bucketBasePath = `cards/${collectionName}/${urlWithoutExtension}`
-        const bucketBaseUrl = `${TEBI_URL}/${TEBI_MHHC_BUCKET_ID}/${bucketBasePath}`;
-
-        return {
-            bucketBasePath,
-            githubUrl: `https://github.com/GrenderG/MHHC_Archive/blob/master/Card%20Scans/${encodeURIComponent(collectionName)}/${encodeURIComponent(cardName)}?raw=true`,
-            name: urlWithoutExtension,
-            largeUrl: `${bucketBaseUrl}_480.webp`,
-            mediumUrl: `${bucketBaseUrl}_320.webp`,
-            smallUrl: `${bucketBaseUrl}_180.webp`,
-        };
-    }
-
-    const text = await request.text();
-    const { commits } = JSON.parse(text);
-
-    try {
-        checkValidity(text);
-    } catch(e) {
-        error(500, (e as Error).message);
-    }
-
-    console.log('Received github payload, starting card transforming');
-
-    const files = getFilesFromCommits(commits);
-
-    for (const file of files) {
-        const card = buildCardFromFile(file);
-        console.log(`Processing file ${card.name}...`)
-        fetchCard(card, fetch, true).then(
-            () => console.log(`File ${card.name} uploaded after push!`)
-        );
-    }
-
-    return new Response();
+    error(500, 'Not available');
+    //
+    // const checkValidity = (textBody: string) => {
+    //     if (!textBody) {
+    //         throw new Error('Unable validate signature');
+    //     }
+    //
+    //     const signatureHeaderName = 'X-Hub-Signature-256';
+    //     const signatureHashAlgorithm = 'sha256';
+    //     const signature = Buffer.from(request.headers.get(signatureHeaderName) ?? '', 'utf8');
+    //     const hmac = createHmac(signatureHashAlgorithm, GITHUB_WEBHOOK_SECRET);
+    //     const digest = Buffer.from(signatureHashAlgorithm + '=' + hmac.update(textBody).digest('hex'), 'utf8');
+    //
+    //     if (signature.length !== digest.length || !timingSafeEqual(digest, signature)) {
+    //         throw new Error('Unable validate signature');
+    //     }
+    // }
+    //
+    // const getFilesFromCommits = (commits: Commit[]): string[] => {
+    //     return commits.reduce((previousValue, currentValue) => {
+    //         return [
+    //             ...previousValue,
+    //             ...currentValue.modified,
+    //             ...currentValue.added,
+    //         ];
+    //     }, [] as string[]);
+    // }
+    //
+    // const buildCardFromFile = (file: string): CardModel => {
+    //     const splitted = file.split('/').filter(s => s !== '');
+    //     const cardName = splitted.at(-1)!;
+    //     const collectionName = splitted.at(-2)!;
+    //
+    //     const urlWithoutExtension = cardName.replace(/\.[^/.]+$/, "");
+    //     const bucketBasePath = `cards/${collectionName}/${urlWithoutExtension}`
+    //     const bucketBaseUrl = `${TEBI_URL}/${TEBI_MHHC_BUCKET_ID}/${bucketBasePath}`;
+    //
+    //     return {
+    //         bucketBasePath,
+    //         githubUrl: `https://github.com/GrenderG/MHHC_Archive/blob/master/Card%20Scans/${encodeURIComponent(collectionName)}/${encodeURIComponent(cardName)}?raw=true`,
+    //         name: urlWithoutExtension,
+    //         largeUrl: `${bucketBaseUrl}_480.webp`,
+    //         mediumUrl: `${bucketBaseUrl}_320.webp`,
+    //         smallUrl: `${bucketBaseUrl}_180.webp`,
+    //     };
+    // }
+    //
+    // const text = await request.text();
+    // const { commits } = JSON.parse(text);
+    //
+    // try {
+    //     checkValidity(text);
+    // } catch(e) {
+    //     error(500, (e as Error).message);
+    // }
+    //
+    // console.log('Received github payload, starting card transforming');
+    //
+    // const files = getFilesFromCommits(commits);
+    //
+    // for (const file of files) {
+    //     const card = buildCardFromFile(file);
+    //     console.log(`Processing file ${card.name}...`)
+    //     fetchCard(card, fetch, true).then(
+    //         () => console.log(`File ${card.name} uploaded after push!`)
+    //     );
+    // }
+    //
+    // return new Response();
 }
